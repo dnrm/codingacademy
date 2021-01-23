c596a22f225ecf72420fce211ece292a83c6f353
Daniel Medina <daniel@medina.com>
2021-01-23 13:06:23 -0600

    hola sofi abre esto es important maso lol. Ok mira ve a este link de aqui bc hay un message https://docs.google.com/document/d/1hQDDYHMm_ZAnfsIYsxKts9Xl5VXWY5y8j1AG055j06s/edit

---
+++ js/Link.js
@@ -0,0 +1,7 @@
+(function() {
+    const el = document.createElement('a');
+    el.href = './';
+    el.innerHTML = 'Go to index';
+    el.classList.add('index')
+    document.body.append(el);
+})();
